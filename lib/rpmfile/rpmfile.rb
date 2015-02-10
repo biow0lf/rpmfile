c594518d30d9ef755d1d99a296cbeaabbe180adf
Igor Zubkov <igor.zubkov@gmail.com>
2015-02-10 04:12:04 +0200

    More refactor

--- lib/rpmfile/rpmfile.rb
+++ lib/rpmfile/rpmfile.rb
@@ -3,11 +3,13 @@ require 'tempfile'
 
 require_relative './basic'
 require_relative './changelog'
+require_relative './extra'
 
 module RPM
   class File
     include Basic
     include Changelog
+    include Extra
 
     # Public: Returns rpm file name as String.
     attr_reader :file
@@ -25,21 +27,6 @@ module RPM
       @source = source
     end
 
-    # Public: Return package epoch:version-release from rpm file.
-    #
-    # Examples
-    #
-    #   evr()
-    #   # => "2:1.26-31.fc20"
-    #
-    #   evr()
-    #   # => "4.2.47-3.fc20"
-    #
-    # Returns package epoch-version-release as String.
-    def evr
-      @evr ||= read_tag('EVR')
-    end
-
     # TODO: EXCLUDEARCH
     #   EXCLUDEOS
 
@@ -52,66 +39,6 @@ module RPM
     # end
 
     # TODO: EXCLUSIVEOS
-
-    # Public: Return package name-epoch:version-release from rpm file.
-    #
-    # Examples
-    #
-    #   nevr()
-    #   # => "tar-2:1.26-31.fc20"
-    #
-    #   nevr()
-    #   # => "bash-4.2.47-3.fc20"
-    #
-    # Returns name-epoch:version-release as String.
-    def nevr
-      @nevr ||= read_tag('NEVR')
-    end
-
-    # Public: Return package name-epoch:version-release.arch from rpm file.
-    #
-    # Examples
-    #
-    #   nevra()
-    #   # => "tar-2:1.26-31.fc20.armv7hl"
-    #
-    #   nevra()
-    #   # => "bash-4.2.47-3.fc20.armv7hl"
-    #
-    #   nevra()
-    #   # => nil
-    #
-    # Returns name-epoch:version-release.arch as String or nil if package is source rpm.
-    def nevra
-      @nevra ||= read_tag('NEVRA') unless source
-    end
-
-    # Public: Return package name-version-release from rpm file.
-    #
-    # Examples
-    #
-    #   nvr()
-    #   # => "tar-1.26-31.fc20"
-    #
-    # Returns name-version-release as String.
-    def nvr
-      @nvr ||= read_tag('NVR')
-    end
-
-    # Public: Return package name-version-release.arch from rpm file.
-    #
-    # Examples
-    #
-    #   nvra()
-    #   # => "tar-1.26-31.fc20.armv7hl"
-    #
-    #   nvra()
-    #   # => nil
-    #
-    # Returns name-version-release.arch as String or nil if source rpm.
-    def nvra
-      @nvra ||= read_tag('NVRA') unless source
-    end
 
     # Public: Return package OPTFLAGS from rpm file.
     #
